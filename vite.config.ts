749fa73715bc4601d40be5f6a1f148ecc76638c2
Iván Sánchez Vázquez <ivan.sanchez@toldosgomez.local>
2026-03-27 10:08:29 +0100

    Config: Simplificación total de Vite para estabilización

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,8 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
+    host: true,
     port: 5173,
-    strictPort: true,
-    hmr: {
-      host: '192.168.4.90',
-      port: 5173,
-    },
     cors: true,
   }
 })
