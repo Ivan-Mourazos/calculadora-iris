4dedeab80d4583c219e4aa053726076bb6f36ff9
Iván Sánchez Vázquez <ivan.sanchez@toldosgomez.local>
2026-03-27 10:05:33 +0100

    Config: Forzado HMR a IP 192.168.4.90

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,13 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
+    host: '0.0.0.0',
     port: 5173,
-    strictPort: false,
-    cors: true
+    strictPort: true,
+    hmr: {
+      host: '192.168.4.90',
+      port: 5173,
+    },
+    cors: true,
   }
 })
